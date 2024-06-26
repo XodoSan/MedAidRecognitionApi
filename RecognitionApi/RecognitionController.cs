164c6118080ec563fc18aee5deac9d8b9cb1c81b
@shvetsov <shvetsov@itopcase.ru>
2024-06-26 10:28:46 +0300

    init

---
+++ RecognitionApi/RecognitionController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecognitionApi
+{
+    [Controller]
+    [Route("api/[controller]")]
+    public class RecognitionController : ControllerBase
+    {
+        public IActionResult GetResult()
+        {
+            return Ok("Порез");
+        }
+    }
+}
