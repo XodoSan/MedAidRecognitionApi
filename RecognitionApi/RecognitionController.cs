ad359c5fd9893112053e52061691b9f5a40ecb79
@shvetsov <shvetsov@itopcase.ru>
2024-06-27 03:28:18 +0300

    Change contract

--- RecognitionApi/RecognitionController.cs
+++ RecognitionApi/RecognitionController.cs
@@ -6,13 +6,27 @@ namespace RecognitionApi
     [Route("api/[controller]")]
     public class RecognitionController : ControllerBase
     {
-        public IActionResult GetResult()
+        [HttpPost]
+        public IActionResult GetResult([FromBody] string[] links)
         {
+            List<MockModel> mockModels = new();
             List<string> injuries = new List<string> { "Порез", "Ушиб" };
             Random random = new Random();
-            int index = random.Next(injuries.Count);
-            string randomInjury = injuries[index];
-            return Ok(randomInjury);
+
+            for (int i = 0; i < 3; i++)
+            {
+                int index = random.Next(injuries.Count);
+                string randomInjury = injuries[index];
+                mockModels.Add(new MockModel { FileName = Guid.NewGuid().ToString() + ".jpg", InjuryType = randomInjury});
+            }
+
+            return Ok(mockModels);
+        }
+
+        public class MockModel
+        {
+            public string FileName { get; set; }
+            public string InjuryType { get; set; }
         }
     }
 }
