32c89d5d6201109c15e9327bfe519d9bcf54c5e6
@shvetsov <shvetsov@itopcase.ru>
2024-06-28 06:27:06 +0300

    fix

--- RecognitionApi/testScript.py
+++ RecognitionApi/testScript.py
@@ -8,4 +8,5 @@ directory = 'C:/Users/Andrew/Desktop/InputPictures/'
 files = os.listdir(directory)
 
 for item in files:
-	model.predict(directory + item, save=True, imgsz=320, hide_conf=True, hide_labels=True)
+	model.predict(directory + item, save=True, imgsz=320, hide_conf=True, hide_labels=True, project='/Users/n.n.anikin/MedAidRecognitionApi
+')
